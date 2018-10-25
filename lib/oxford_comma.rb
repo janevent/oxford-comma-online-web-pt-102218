36e95b962d13007733a50e1102977607ea82ed88
Jane Venter <janeanneventer@gmail.com>
2018-10-25 16:34:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   else
     array.each do |e|
-      array[-1] = "and #{e}"
+      array.length -1 = "and #{e}"
     end
     array.join(", ")
   end
