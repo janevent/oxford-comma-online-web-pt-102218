5e31e71a31468183048b72f6d0c93cf7471f9456
Jane Venter <janeanneventer@gmail.com>
2018-10-25 16:32:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,10 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   else
+    array.each do |e|
+      array[-1] = "and #{e}"
+    end
     array.join(", ")
+  end
   
 end
